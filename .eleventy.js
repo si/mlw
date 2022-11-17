82720ece883f0ca21e7c37be34fd08b48deb99ce
Si Jobling <si@jobling.com>
2022-11-17 21:07:05 +0000

    Set navigation to seasons list

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,17 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "src/images" : "images"});
     eleventyConfig.addPassthroughCopy({ "src/admin" : "admin"});
 
+    eleventyConfig.addCollection("seasonTagsList", function(collectionApi) {
+        const tagsList = new Set();
+        collectionApi.getAll().map( item => {
+            if (item.data.tags) { // handle pages that don't have tags
+                //item.data.tags.map( tag => tagsList.add(tag))
+                item.data.tags.filter(a => a.startsWith('season')).map( tag => tagsList.add(tag))
+            }
+        });
+        return tagsList;
+    });
+
     return {
         dir: {
             input: "src/views",
